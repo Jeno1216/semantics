7a505898b3edc47c450ad2811046bac6d49395cd
Jeno1216 <80746647+Jeno1216@users.noreply.github.com>
2023-04-24 17:24:02 +0800

    Update semantics.py

--- semantics.py
+++ semantics.py
@@ -122,8 +122,8 @@ def app():
             st.text(df.shape)
             
             #Randomly select samples
-            label_0=df[df['label']==0].sample(n=23)
-            label_1=df[df['label']==1].sample(n=23)
+            label_0=df[df['label']==0].sample(n=5)
+            label_1=df[df['label']==1].sample(n=5)
             
             train = pd.concat([label_1, label_0])
 
